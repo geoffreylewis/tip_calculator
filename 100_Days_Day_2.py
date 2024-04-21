5dbb6d91df5efde05ceb857aeaf62d8fdb3ef72e
Geoff Lewis <34752513+geoffreylewis@users.noreply.github.com>
2024-04-21 08:19:44 -0500

    Add files via upload
    Program is working.

--- 100_Days_Day_2.py
+++ 100_Days_Day_2.py
@@ -7,10 +7,15 @@ print("Welcome to the \"Tip Calculator\".  Do you dislike having to calculate th
 print("Well, I got you covered.\n")
 
 # Questions
-bill_before_tip = input("First question: What's the total damage (before or after tax, your choice)?  *NOTE: Only type the number and not the \"$\" sign, please.\n")
-people_splitting = input("Second question: How many people are splitting the bill including yourself?  *NOTE: Only type a number and not a word, please.\n")
-tip_possibility = input("Third and final question: Do you want to leave 15%, 20%, or 25% tip?  *NOTE: Only type the number and not the \"%\" sign, please.\n")
+bill_before_tip = int(input("First question: What's the total damage (before or after tax, your choice)?  *NOTE: Only type the number and not the \"$\" sign, please.\n"))
+people_splitting = int(input("Second question: How many people are splitting the bill including yourself?  *NOTE: Only type a number and not a word, please.\n"))
+tip_possibility = int(input("Third and final question: Do you want to leave 15%, 20%, or 25% tip?  *NOTE: Only type the number and not the \"%\" sign, please.\n"))
 
-# Tip and Total Bill
+# New Calculated Values
+share_of_bill = round(bill_before_tip / people_splitting, 2)
+tip = round((tip_possibility / 100) * share_of_bill, 2)
+total_damage = round(share_of_bill + tip, 2)
+
+# Tip and Total Bill (using f-String)
 print()
-print("Well, your share of the bill is $''; with a tip of $'' added on, the total damage looks to be $''.  ...Lucky you.")
+print(f"Well, your share of the bill is ${share_of_bill}; with a tip of ${tip} added on, the total damage looks to be ${total_damage} when this meal is said and done.  ...Lucky you.")
